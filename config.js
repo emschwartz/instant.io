50eae63c88be4d0f9ff1f50b904d20cf61e56c65
Evan Schwartz <evan.mark.schwartz@gmail.com>
2016-04-08 17:08:54 -0700

    Change config to use env vars

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.isProd = process.env.NODE_ENV === 'production'
-exports.host = exports.isProd && '23.239.22.146'
+exports.host = process.env.HOST || exports.isProd && '23.239.22.146'
 exports.ports = {
-  http: exports.isProd ? 80 : 9100,
-  https: exports.isProd ? 443 : 9101
+  http: process.env.PORT_HTTP || exports.isProd ? 80 : 9100,
+  https: process.env.PORT_HTTPS || exports.isProd ? 443 : 9101
 }
